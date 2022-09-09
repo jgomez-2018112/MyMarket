aad25b122c0541b266433bd34b10762b5ac0008c
Eduardo Hernandez <ehernandez-2021591@kinal.edu.gt>
2022-09-08 19:17:46 -0600

    Se arreglo un error en categori

--- MyMarket/src/java/modelo/Categoria.java
+++ MyMarket/src/java/modelo/Categoria.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
-/**
- *
- * @author User
- */
+
 public class Categoria {
+    private int codigoCategoria;
+    private String nombreCategoria;
+
+    public Categoria() {
+    }
+
+    public Categoria(int codigoCategoria, String nombreCategoria) {
+        this.codigoCategoria = codigoCategoria;
+        this.nombreCategoria = nombreCategoria;
+    }
+
+    public int getCodigoCategoria() {
+        return codigoCategoria;
+    }
+
+    public void setCodigoCategoria(int codigoCategoria) {
+        this.codigoCategoria = codigoCategoria;
+    }
+
+    public String getNombreCategoria() {
+        return nombreCategoria;
+    }
+
+    public void setNombreCategoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
     
 }
--- MyMarket/src/java/modelo/CategoriaDAO.java
+++ MyMarket/src/java/modelo/CategoriaDAO.java
@@ -1,10 +1,39 @@
+  package modelo;
 
-package modelo;
+import config.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- *
- * @author User
- */
 public class CategoriaDAO {
-    
+    Conexion cn = new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+  
+  
+ 
+      public List listar(){
+               String sql="Select * From Categorias";
+                  List<Categoria>listaCategoria=new ArrayList<>();
+                          try{
+                             con=cn.Conexion();
+                             ps=con.prepareStatement(sql);
+                             rs=ps.executeQuery();
+                       while(rs.next()){
+                                 Categoria cat=new Categoria();
+                                 cat.setCodigoCategoria(rs.getInt(1));
+                                 cat.setNombreCategoria(rs.getString(2));
+                                 listaCategoria.add(cat);
+                       }
+                   }catch(Exception e){
+                           e.printStackTrace();
+                           }
+                   
+                   return listaCategoria;
+               }
 }
+  
+
